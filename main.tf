c5708d0bb12a2daef3882639ab4df6f320e2711e
Brandon Ferguson <bnferguson@gmail.com>
2020-04-30 15:26:41 +0200

    remove

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-resource "stress_cpu" "my-server" {
-  duration = 60
-}
+#resource "stress_cpu" "my-server" {
+  #duration = 60
+#}
 
-resource "stress_memory" "memory" {
-  duration = 60
-  size = 64
-}
+#resource "stress_memory" "memory" {
+  #duration = 60
+  #size = 64
+#}
