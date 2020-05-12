f43248cc8ee5f9416b415b06ab587a897964e659
Brandon Ferguson <bnferguson@gmail.com>
2020-05-12 15:56:17 +0200

    fire off creation again

--- main.tf
+++ main.tf
@@ -13,11 +13,11 @@ variable "memory_stress_size" {
   default = "1000"
 }
 
-#resource "stress_cpu" "my-server" {
-  #duration = var.cpu_stress_duration
-#}
+resource "stress_cpu" "my-server" {
+  duration = var.cpu_stress_duration
+}
 
-#resource "stress_memory" "memory" {
-  #duration = var.memory_stress_duration
-  #size = var.memory_stress_size
-#}
+resource "stress_memory" "memory" {
+  duration = var.memory_stress_duration
+  size = var.memory_stress_size
+}
