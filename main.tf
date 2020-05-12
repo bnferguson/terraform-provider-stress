46d3e8af09793957cef919794f04ca34d1be590f
Brandon Ferguson <bnferguson@gmail.com>
2020-05-12 16:21:32 +0200

    running with 750

--- main.tf
+++ main.tf
@@ -10,14 +10,14 @@ variable "memory_stress_duration" {
 
 variable "memory_stress_size" {
   type = number
-  default = "500"
+  default = "750"
 }
 
-#resource "stress_cpu" "my-server" {
-  #duration = var.cpu_stress_duration
-#}
+resource "stress_cpu" "my-server" {
+  duration = var.cpu_stress_duration
+}
 
-#resource "stress_memory" "memory" {
-  #duration = var.memory_stress_duration
-  #size = var.memory_stress_size
-#}
+resource "stress_memory" "memory" {
+  duration = var.memory_stress_duration
+  size = var.memory_stress_size
+}
