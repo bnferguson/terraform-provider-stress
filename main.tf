ca551fd842c011176a9c8f5c55b0b186713e5482
Brandon Ferguson <bnferguson@gmail.com>
2020-05-20 13:38:08 +0200

    mvoe to memory only for 512 tests

--- main.tf
+++ main.tf
@@ -10,19 +10,19 @@ variable "memory_stress_duration" {
 
 variable "memory_stress_size" {
   type = number
-  default = "300"
+  default = "400"
 }
 
 #resource "stress_cpu" "my-server" {
   #duration = var.cpu_stress_duration
 #}
 
-#resource "stress_memory" "memory" {
-  #duration = var.memory_stress_duration
-  #size = var.memory_stress_size
-#}
-
-resource "stress_statefile" "statefile" {
-  count = 5000
-  size = 1 # size is currently not used
+resource "stress_memory" "memory" {
+  duration = var.memory_stress_duration
+  size = var.memory_stress_size
 }
+
+#resource "stress_statefile" "statefile" {
+  #count = 5000
+  #size = 1 # size is currently not used
+#}
